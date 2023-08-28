93b011390dfc42da0bc0e75512f96710f05cf7ff
Blaze Dowis <blazedowis@gmail.com>
2023-08-28 13:58:33 -0700

    Initial Commit

---
+++ MessengerClone/MessengerCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerCloneApp.swift
+//  MessengerClone
+//
+//  Created by Blaze Dowis on 8/28/23.
+//
+
+import SwiftUI
+
+@main
+struct MessengerCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
